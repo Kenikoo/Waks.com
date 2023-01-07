a17ebab644da543cdc95158f2240e33bda48b4b3
Kenikoo <120331152+Kenikoo@users.noreply.github.com>
2023-01-07 20:25:04 +0100

    Add files via upload

---
+++ Conditional Statement.js
@@ -0,0 +1,37 @@
+
+//If-Else statment is use to execute         the code wwether the condition is true or false
+
+/*let a = 5;
+let b = 4
+let result = a >= b
+
+if (result)
+console.log("a is greater");
+
+else
+console.log("b is greater");
+
+console.log("bye......");*/
+
+/*var c = 5;
+var d = 3;
+if (c > d){
+    console.log("c is greater");
+    console.log("bye.......");
+}
+    
+  
+else {
+    console.log("d is greater");
+    console.log('yece...')
+
+}*/
+    
+
+
+let i = 1;
+while (i<=10)
+{
+    console.log("Hi");
+    --i
+}
